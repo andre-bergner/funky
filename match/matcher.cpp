072b562b5a3d3ce3d10f6e7e5a887a78b34eea3b
André Bergner <andre.bergner.0@gmail.com>
2017-06-29 22:18:25 +0200

    Adding working case_ notation version (prototype).

--- match/matcher.cpp
+++ match/matcher.cpp
@@ -6,6 +6,7 @@ using int_ = std::integral_constant<int,N>;
 
 #include <iostream>
 
+
 int main()
 {
    using namespace std;
@@ -20,15 +21,27 @@ int main()
       ,  [&](int_<7>){ cout << "7even" << endl; }
       );
 #else
-   volatile int k;
    volatile int n = 3;
-      match(n)
-      (  [&](int_<4>){ k = 314; }
-      ,  [&](int_<6>){ k = 47; }
-      ,  [&](int_<1>){ k = 1337; }
-      ,  [&](int_<3>){ k = 7357; }
-      ,  [&](int_<7>){ k = -42; }
+   volatile auto k
+   = match(n)
+      (  [](int_<4>){ return 314; }
+      ,  [](int_<6>){ return 47; }
+      ,  [](int_<1>){ return 1337; }
+      ,  [](int_<3>){ return 7357; }
+      ,  [](int_<7>){ return -42; }
       );
 #endif
+
+   std::cout << "----- match 2 -----" << std::endl;
+
+   volatile auto x = 4.1;
+   auto result = match2(x)
+   (  case_(4.1) >= []{ return 314; }
+   ,  case_(6.1) >= []{ return 47; }
+   ,  case_(1.1) >= []{ return 1337; }
+   ,  case_(3.1) >= []{ return 7357; }
+   ,  case_(7.1) >= []{ return -42; }
+   );
+   std::cout << *result << std::endl;
 }
 
--- match/matcher.hpp
+++ match/matcher.hpp
@@ -27,3 +27,78 @@ constexpr auto match(int x)
       return dispatch(std::forward<decltype(lambdas)>(lambdas)...)(x);
    };
 }
+
+
+
+
+
+
+template <typename Value, typename Lambda>
+struct match_pair;
+
+
+// alternative names
+// • token     / tok
+// • pattern   / pat
+// • value     / val
+// • condition / cond
+
+template <typename Value>
+struct case_t
+{
+   const Value value;
+};
+
+template <typename Value>
+auto case_(Value&& x) -> case_t<Value>
+{ return {std::forward<Value>(x)}; }
+
+
+template <typename Value, typename Lambda>
+struct match_pair
+{
+   Value    value;
+   Lambda   lambda;
+};
+
+
+template <typename Value, typename Lambda>
+auto operator >= (case_t<Value>&& c, Lambda&& lambda) -> match_pair<Value,Lambda>
+{
+   return { std::move(c.value), std::forward<Lambda>(lambda) };
+}
+
+
+
+
+namespace detail
+{
+   template <typename ReturnType, typename X>
+   constexpr auto linear_dispatch(X&&) -> opt_result_t<ReturnType>
+   {
+      return {};
+   }
+
+   template <typename ReturnType, typename X, typename Case, typename... Cases>
+   constexpr auto linear_dispatch(X const& x, Case&& c, Cases&&... cs) -> opt_result_t<ReturnType>
+   {
+      if (c.value == x)
+         return invoker<ReturnType>::apply(c.lambda);
+      else
+         return linear_dispatch<ReturnType>( x, std::forward<Cases>(cs)... );
+   }
+}
+
+
+
+
+template <typename Value>
+constexpr auto match2(Value&& x)
+{
+   return [x=std::forward<Value>(x)](auto&&... cases)
+   {
+      using result_t = typename detail::result<decltype(cases.lambda)...>::type;
+      return detail::linear_dispatch<result_t>( x, std::forward<decltype(cases)>(cases)... );
+   };
+}
+
